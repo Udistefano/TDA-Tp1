295dd79515371642f18e81110ceadbbfe31bed01
Udistefano <udistefano@fi.uba.ar>
2025-04-13 18:34:27 -0300

    agrego set de datos problema 2

--- problema2Greedy.py
+++ problema2Greedy.py
@@ -15,21 +15,27 @@ def carga_agua(distancias_estaciones, autonomia):
     distancia = distancias_estaciones[i] 
     while i < len(distancias_estaciones):
         distancia = distancias_estaciones[i]
+        if distancia > autonomia:
+            raise ValueError(f"La distancia entre estaciones ({distancia} km) excede la autonomía ({autonomia} km).")
         if tanque < distancia:
             estaciones_convenientes.append(km_actual)
             tanque = autonomia
-        if distancia > autonomia:
-            raise ValueError(f"La distancia entre estaciones ({distancia} km) excede la autonomía ({autonomia} km).")
-        else:
-            tanque -= distancia
-            km_actual += distancia
-            i += 1
+        tanque -= distancia
+        km_actual += distancia
+        i += 1
     return estaciones_convenientes
 
-distancias = [5, 3, 3, 4, 7, 10]
+# Sets de datos para probar codigo
+distancias1 = [5, 3, 3, 4, 7, 2, 3, 4]
+distancias2 = [5, 3, 2]
+distancias3 = [5, 3, 3, 4, 7, 2, 3, 4, 6, 3, 6, 4, 4, 2, 7, 7, 3, 5]
+#set de datos para corroborar la excepción
+# distancias4 = [5, 3, 3, 4, 8, 2, 3, 4]
 
-print(carga_agua(distancias, 7))
-
+print(carga_agua(distancias1, 7))
+print(carga_agua(distancias2, 7))
+print(carga_agua(distancias3, 7))
+#print(carga_agua(distancias4, 7))
 # complejidad O(n)
 # La complejidad óptima para este tipo de problema es O(n).
 # este algoritmo greedy sí encuentra el óptimo para este problema porque cumple con la propiedad del subproblema óptimo
